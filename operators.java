6160728eaae2461f3bafdfa2c822fa9c052aa183
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-09-19 13:23:45 -0400

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,17 @@
+class Main { 
+
+    public static void main(String[] args) {
+
+        int a,b; //declaration
+        a = 6;   //initialization
+        b = 3; 
+        int x = a/b; //module operator (remainder)
+        int y=a/b;
+        double z= a/b;
+        System.out.println("Integer value is " +x);
+        System.out.println("double value is " +z);
+        System.out.println(x == z);
+       
+
+    }
+}
